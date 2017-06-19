6df265418903444d0f7dad0c591b4504d0e903fc
Brad Rosen <nywebman2@yahoo.com>
2017-06-19 16:39:14 -0400

    first action, reducer, and set up store

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import courses from './couseReducer';
+
+const rootReducer = combineReducers({
+    courses //aliased above, in es6 if both sides are the same can do it like this instead of  courses:courses. short hand property name
+});
+
+export default rootReducer;
