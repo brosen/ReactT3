3ce0cda98bf57b8cff5589d922ebdea0f972037d
Brad Rosen <brosen@mdsol.com>
2017-06-20 12:26:13 -0400

    Thunk  add to store  create load course thunk  load courses in reducer  dispatch action create CourseList component

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,5 +1,20 @@
 import * as types from './actionTypes';
+import CourseApi from '../api/mockCourseApi';
 
-export function createCourse(course) {  //Action creator
-    return { type: types.CREATE_COURSE, course };
+export function loadCoursesSuccess(courses) { 
+    return { type: types.LOAD_COURSES_SUCCESS, courses };
+}
+
+// Functions below handle asynchronous calls.
+// Each returns a function that accepts a dispatch.
+// These are used by redux-thunk to support asynchronous interactions.
+export function loadCourses() {
+    return function (dispatch) {
+        //dispatch(beginAjaxCall());
+        return CourseApi.getAllCourses().then(courses => {
+            dispatch(loadCoursesSuccess(courses));
+        }).catch(error => {
+            throw (error);
+        });
+    };
 }
