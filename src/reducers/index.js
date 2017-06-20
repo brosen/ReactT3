6e85f23eef61743945f793aab64d25b12b856d40
Brad Rosen <brosen@mdsol.com>
2017-06-20 13:04:51 -0400

    create the course page and wire up action, reducer, and map data for dropdown

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import courses from './courseReducer';
+import authors from './authorReducer';
 
 const rootReducer = combineReducers({
-    courses //aliased above, in es6 if both sides are the same can do it like this instead of  courses:courses. short hand property name
+    courses, //aliased above, in es6 if both sides are the same can do it like this instead of  courses:courses. short hand property name
+    authors
 });
 
 export default rootReducer;
